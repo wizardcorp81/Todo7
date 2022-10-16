31deb9a7dad49ec26b513d961a5cf7b80bfe0071
Alvaro Machado de Lima <alvaro.lima@lojasrenner.com.br>
2022-10-16 18:42:47 -0300

    Correção do Async

--- test/calculadora.test.js
+++ test/calculadora.test.js
@@ -1,22 +1,22 @@
 
 const calculadora = require("../calculadora");
 
-test("deve somar 2 numeros corretamente",() => {
+test("deve somar 2 numeros corretamente",async() => {
     const res = await requestAnimationFrame(calculadora).post('/somar')
     expect(res.statusCode).toEqual(200)
     expect(res.body).toHaveProperty('total');
 });
-test("deve subtrair 2 numeros corretamente",() => {
+test("deve subtrair 2 numeros corretamente",async() => {
     const res = await requestAnimationFrame(calculadora).post('/subtrair')
     expect(res.statusCode).toEqual(200)
     expect(res.body).toHaveProperty('total');
 });
-test("deve dividir 2 numeros corretamente",() => {
+test("deve dividir 2 numeros corretamente",async() => {
     const res = await requestAnimationFrame(calculadora).post('/dividir')
     expect(res.statusCode).toEqual(200)
     expect(res.body).toHaveProperty('total');
 });
-test("deve multiplicar 2 numeros corretamente",() => {
+test("deve multiplicar 2 numeros corretamente",async() => {
     const res = await requestAnimationFrame(calculadora).post('/multiplicar')
     expect(res.statusCode).toEqual(200)
     expect(res.body).toHaveProperty('total');
